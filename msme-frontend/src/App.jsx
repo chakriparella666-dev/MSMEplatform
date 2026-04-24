17d60dd95508fa569a87054c13ab00eee31603a3
pavansai2608 <golipavansaikrishna2608@gmail.com>
2026-04-24 20:43:11 +0530

    feat: add RouteWithTitle wrapper for dynamic per-route document titles

--- msme-frontend/src/App.jsx
+++ msme-frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import { AuthProvider } from './context/AuthContext'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -19,6 +21,17 @@ import Profile from './pages/buyer/Profile'
 import PWAInstallPrompt from './components/PWAInstallPrompt'
 import ErrorBoundary from './components/ErrorBoundary'
 
+function RouteWithTitle({ element, title }) {
+  useEffect(() => {
+    if (title) document.title = `${title} | MSMEMarket`
+  }, [title])
+  return element
+}
+RouteWithTitle.propTypes = {
+  element: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
 function App() {
   return (
     <AuthProvider>
@@ -26,27 +39,27 @@ function App() {
       <ErrorBoundary>
         <Routes>
           <Route path="/" element={<Navigate to="/buyer" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<RouteWithTitle element={<Login />} title="Login" />} />
+          <Route path="/register" element={<RouteWithTitle element={<Register />} title="Register" />} />
           <Route path="/dashboard" element={<Navigate to="/buyer" replace />} />
 
           {/* Buyer Routes */}
-          <Route path="/buyer" element={<BuyerDashboard />} />
+          <Route path="/buyer" element={<RouteWithTitle element={<BuyerDashboard />} title="Shop" />} />
           <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/cart" element={<RouteWithTitle element={<CartPage />} title="Cart" />} />
+          <Route path="/checkout" element={<RouteWithTitle element={<Checkout />} title="Checkout" />} />
           <Route path="/order-success" element={<OrderSuccess />} />
-          <Route path="/my-orders" element={<MyOrders />} />
-          <Route path="/addresses" element={<Addresses />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/my-orders" element={<RouteWithTitle element={<MyOrders />} title="My Orders" />} />
+          <Route path="/addresses" element={<RouteWithTitle element={<Addresses />} title="Addresses" />} />
+          <Route path="/wishlist" element={<RouteWithTitle element={<Wishlist />} title="Wishlist" />} />
+          <Route path="/profile" element={<RouteWithTitle element={<Profile />} title="Profile" />} />
 
           {/* Workspace Routes */}
-          <Route path="/seller" element={<SellerDashboard />} />
-          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/seller" element={<RouteWithTitle element={<SellerDashboard />} title="Seller Hub" />} />
+          <Route path="/admin" element={<RouteWithTitle element={<AdminDashboard />} title="Admin" />} />
 
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<RouteWithTitle element={<ForgotPassword />} title="Forgot Password" />} />
+          <Route path="/reset-password/:token" element={<RouteWithTitle element={<ResetPassword />} title="Reset Password" />} />
 
           {/* Catch-all for undefined routes */}
           <Route path="*" element={<Navigate to="/login" replace />} />
